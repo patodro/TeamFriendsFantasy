f5b8731a9a5cd7f1b34507423733ad37e4dd1b98
patodro <patodro@gmail.com>
2024-07-19 21:44:03 -0400

    rename example to GoodellBot

--- commish/Goodellbot.py
+++ commish/Goodellbot.py
@@ -5,10 +5,17 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+commishChannelID = 1256234378443362376
+generalChannelID = 1237187300547367036
+infoChannelID = 1256224215011557448
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
     print('------')
+    for server in client.guilds:
+        for channel in server.channels:
+            print(f'{channel.type} | {channel} | {channel.id}')
 
 @client.event
 async def on_message(message):
